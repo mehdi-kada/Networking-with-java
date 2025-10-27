40772a259f6cff12dfed13774eb65001f4591aa6
mehdi-kada <mehdikada64@gmail.com>
2025-10-27 20:08:21 +0100

    Implement basic server functionality with socket communication

--- src/main/java/com/sockets/Server.java
+++ src/main/java/com/sockets/Server.java
@@ -4,7 +4,38 @@ import java.io.*;
 import java.net.*;
 
 public class Server{
+    private static final int PORT = 8080;
     public static void main(String[] args) throws Exception{
-        
+        try{
+            ServerSocket socket = new ServerSocket(PORT);
+            Socket clientSocket = socket.accept();
+            BufferedReader buffIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter buffOut = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            while (true) {
+                String msgFromClient = buffIn.readLine();
+                if (msgFromClient == null) {
+                    System.out.println("Client disconnected.");
+                    break;
+                }
+                System.out.println("Client: " + msgFromClient);
+                buffOut.write("msg received");
+                buffOut.newLine();
+                buffOut.flush();
+
+                if (msgFromClient.equalsIgnoreCase("BYE")) {
+                    break;
+                }
+            }
+            
+            clientSocket.close();
+            buffIn.close();
+            buffOut.close();
+            socket.close();
+        }catch (IOException e) {
+            System.err.println("I/O error occurred.");
+            e.printStackTrace();
+        }
+        System.out.println("Server closed.");
     }
 }
